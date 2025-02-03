84a9d6061c7beffe14e4d1b0f2c20fc19c5fe28b
Teo <90730116+C-Teo@users.noreply.github.com>
2025-02-03 00:42:29 -0500

    feat: navbar and image list

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,32 @@
 import React from 'react';
 import {
-  Container,
-  Grid,
-  Card,
-  CardMedia,
-  CardContent,
+  Grid2,
   Typography,
+  ImageList,
+  ImageListItem,
 } from '@mui/material';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
 
-const artworks = [
-  {
-    title: 'Artwork 1',
-    description: 'A beautiful piece of abstract art.',
-    image: '../images/1.JPG',
-  },
-  {
-    title: 'Artwork 2',
-    description: 'An intriguing landscape artwork.',
-    image: '../images/2.png',
-  },
-  {
-    title: 'Artwork 3',
-    description: 'A captivating modern art piece.',
-    image: '../images/3.png',
-  },
-  {
-    title: 'Artwork 4',
-    description: 'A classic portrait with a twist.',
-    image: '../images/4.JPG',
-  },
-];
+const images = import.meta.glob('/images/*.{png,jpg,jpeg,svg}', { eager: true });
 
 function App() {
+  console.log(Object.keys(images))
   return (
-    <Container sx={{ py: 4 }}>
-      <Typography variant="h2" align="center" gutterBottom>
-        Art Portfolio
-      </Typography>
-      <Grid container spacing={4}>
-        {artworks.map((art, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <Card>
-              <CardMedia
-                component="img"
-                height="200"
-                image={art.image}
-                alt={art.title}
-              />
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  {art.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {art.description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+    <Grid2>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <ImageList variant="masonry" cols={4} gap={8} sx={{ paddingInline: '5rem' }}>
+        {Object.keys(images).map((image, index) => (
+          <ImageListItem key={index}>
+            <img
+              srcSet={`${image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${image}?w=248&fit=crop&auto=format`}
+              alt={image}
+              loading="lazy"
+            />
+          </ImageListItem>
         ))}
-      </Grid>
-    </Container>
+      </ImageList>
+    </Grid2>
   );
 }
 
